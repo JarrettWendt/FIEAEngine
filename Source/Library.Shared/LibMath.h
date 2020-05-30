dadf6154906eb9100cdde5be05409d002a57c7e0
Jarrett Wendt <wendt.j@icloud.com>
2020-05-29 20:46:10 -0400

    pi

--- Source/Library.Shared/LibMath.h
+++ Source/Library.Shared/LibMath.h
@@ -9,40 +9,40 @@ namespace Library::Math::Constants
 {
 	// these constants were retrieved from WolframAlpha
 	constexpr long double PI_llf = 3.141592653589793238462643383279502884197169399375105820974L;
-	constexpr double PI_lf = PI_llf;
-	constexpr float PI_f = PI_llf;
-	constexpr float PI = PI_llf;
+	constexpr double PI_lf = double(PI_llf);
+	constexpr float PI_f = float(PI_llf);
+	constexpr float PI = float(PI_llf);
 
 	// tau is 2pi
 	constexpr long double TAU_llf = 6.283185307179586476925286766559005768394338798750211641949L;
-	constexpr double TAU_lf = TAU_llf;
-	constexpr float TAU_f = TAU_llf;
-	constexpr float TAU = TAU_llf;
+	constexpr double TAU_lf = double(TAU_llf);
+	constexpr float TAU_f = float(TAU_llf);
+	constexpr float TAU = float(TAU_llf);
 
 	// eta is pi/2
 	constexpr long double ETA_llf = 1.570796326794896619231321691639751442098584699687552910487L;
-	constexpr double ETA_lf = ETA_llf;
-	constexpr float ETA_f = ETA_llf;
-	constexpr float ETA = ETA_llf;
+	constexpr double ETA_lf = double(ETA_llf);
+	constexpr float ETA_f = float(ETA_llf);
+	constexpr float ETA = float(ETA_llf);
 }
 
 namespace Library::Math
 {
 	// aliases to Library::Math::Constants for when you don't want to `using namespace` and the fully qualified namespace is too wordy.
-	using PI_llf = Constants::PI_llf;
-	using PI_lf = Constants::PI_lf;
-	using PI_f = Constants::PI_f;
-	using PI = Constants::PI;
+	constexpr auto PI_llf = Constants::PI_llf;
+	constexpr auto PI_lf = Constants::PI_lf;
+	constexpr auto PI_f = Constants::PI_f;
+	constexpr auto PI = Constants::PI;
 
-	using TAU_llf = Constants::TAU_llf;
-	using TAU_lf = Constants::TAU_lf;
-	using TAU_f = Constants::TAU_f;
-	using TAU = Constants::TAU;
+	constexpr auto TAU_llf = Constants::TAU_llf;
+	constexpr auto TAU_lf = Constants::TAU_lf;
+	constexpr auto TAU_f = Constants::TAU_f;
+	constexpr auto TAU = Constants::TAU;
 
-	using ETA_llf = Constants::ETA_llf;
-	using ETA_lf = Constants::ETA_lf;
-	using ETA_f = Constants::ETA_f;
-	using ETA = Constants::ETA;
+	constexpr auto ETA_llf = Constants::ETA_llf;
+	constexpr auto ETA_lf = Constants::ETA_lf;
+	constexpr auto ETA_f = Constants::ETA_f;
+	constexpr auto ETA = Constants::ETA;
 	
 	/**
 	 * wrapper for safely decrementing an unsigned type without underflowing
